4738034485b89209d3c5b8e8a579cc6be852f9df
Carlos Daniel Agudelo <cardaguh@gmail.com>
2017-08-31 01:39:13 -0500

    added test page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { TestPage } from '../test/test';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  showProfilePage() {
+    this.navCtrl.push(TestPage);
   }
 
 }
